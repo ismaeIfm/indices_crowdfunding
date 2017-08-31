15c9225c88418409f68b543d383f095420032108
Ismael <ismaelfm_@ciencias.unam.mx>
2017-08-31 02:33:07 -0500

    Adding acumulado from deuda

--- acumulado_deuda.py
+++ acumulado_deuda.py
@@ -3,17 +3,19 @@ import string
 import numpy as np
 import pandas as pd
 
-donacion = pd.read_csv('data-out/MiCochinitoData.csv')
+donacion = pd.read_csv('data-out/KuboData.csv')
 #donacion = donacion.groupby([u'id', u'cve', u't', 'm', 'id2',
 #                             'DesGeo'])[u'valor'].sum().reset_index(
 #                                 name="valor")
 file_acumulado = []
 acumulado = []
-for y in [2015, 2016, 2017]:
+for y in [2012, 2013, 2014, 2015, 2016, 2017]:
     by_year = donacion.loc[donacion["t"] == y]
     for t in [1, 2, 3, 4]:
         if y == 2017 and t == 4:
             break
+        if y == 2012 and t < 4:
+            continue
         by_quarter = by_year.loc[by_year["m"] == t]
         actual = by_quarter[[u'id', u'cve', 'id2', 'valor', 'DesGeo']]
 
@@ -28,4 +30,4 @@ for y in [2015, 2016, 2017]:
 
         file_acumulado.append(aux)
 
-pd.concat(file_acumulado).to_csv('AcumuladoCochino.csv', index=False)
+pd.concat(file_acumulado).to_csv('AcumuladoKubo.csv', index=False)
